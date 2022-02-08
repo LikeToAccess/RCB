943461846510fd72e295381e4423033ca87eea97
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2022-02-08 05:02:43 -0600

    Updated main.py

--- main.py
+++ main.py
@@ -2,10 +2,34 @@ import format_json
 from database import Database
 
 
+log_filename = "completed.log"
+
+
+def read_file(filename):
+	try:
+		with open(filename, "r") as file:
+			data = file.read().split("\n")
+	except FileExistsError:
+		write_file(filename, "")
+		return read_file(filename)
+	return data
+
+def write_file(filename, data):
+	with open(filename, data) as file:
+		file.write(data)
+
+def append_file(filename, data):
+	with open(filename, "a") as file:
+		file.write(f"{data}\n")
+
 def main():
+	completed = read_file(log_filename)
 	filenames = format_json.run()["files"]
 	for filename in filenames:
-		Database.run(filename)
+		if filename in completed:
+			continue
+		Database(filename).run()
+		append_file(log_filename, filename)
 
 
 if __name__ == "__main__":
